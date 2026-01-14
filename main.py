156a4ca11eb3ef031e0a0325937825ef8bb0f07b
Srinand Hardikar <ghoulm4a1@gmail.com>
2026-01-14 23:55:41 +0530

    Functions Added

--- main.py
+++ main.py
@@ -4,6 +4,45 @@ Income = 0
 Expense = 0
 Stop = True
 
+def add_income(Income):
+    while True:
+        amount = input("Enter the amount: ")
+        try: 
+            amount = float(amount)
+            Income += amount
+            print("Income updated Successfully!")
+            print("Total Income: ", Income)
+            break
+        except:
+            print("Please enter a valid number.")
+    return Income
+
+def add_expense(Expense):
+    while True:
+        amount = input("Enter the amount: ")
+        try: 
+            amount = float(amount)
+            Expense += amount
+            print("Expense Updated Successfully!")
+            print("Total Expense: ", Expense)
+            break
+        except:
+            print("Please enter a valid number.")
+    return Expense
+
+def show_summary(Income, Expense):
+    while True:
+        try:
+            Savings = Income - Expense
+            print("\n ---Summary---")
+            print("Income:",Income)
+            print("Expense:",Expense)
+            print("Savings:", Savings)
+            break
+        except:
+            print("Please enter a valid number.")
+
+
 while Stop:
     print("\nChoose an option:")
     print("Income / Expense / Savings / Quit")
@@ -11,35 +50,14 @@ while Stop:
     choice = input("Enter your choice: ").lower().strip() #used strips so that there shouldn't be any whitespaces remains
 
     if choice == "income":
-        while True:
-            amount = input("Enter the amount: ")
-            try: 
-                amount = float(amount)
-                Income += amount
-                print("Income updated Successfully!")
-                print("Total Income: ",Income)
-                break
-            except:
-                print("Please enter a valid number.")
+        Income = add_income(Income)
     elif choice == "expense":
-        while True:
-            amount = input("Enter the amount: ")
-            try:
-                amount = float(amount)
-                Expense += amount
-                print("Expense updated Successfully!")
-                print("Total Expense: ", Expense)
-                break
-            except:
-                print("Please enter a valid number.")
+        Expense = add_expense(Expense)
     elif choice == "savings":
-        Savings = Income - Expense
-        print("\n ---Summary---")
-        print("Income:",Income)
-        print("Expense:",Expense)
-        print("Savings:", Savings)
-
+        Savings = show_summary(Income, Expense)
     elif choice == "quit":
         print("Thanks for using the Finance Tracker. Goodbye!")
     else:
         print("Invalid choice. Please try again.")
+
+    
